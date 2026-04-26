a22c05ad4ed583ca0f624460f18286b3210a2b59
Gloria Estrada <gloriaestra2@cecytab.edu.mx>
2026-04-25 22:30:48 -0600

    fix mysql env railway

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def bd_teacheasy():
     try:
         conexion = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT", 26878)),
+            port=int(os.getenv("DB_PORT", "26878")),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
