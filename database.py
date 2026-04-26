9c7db60d2bd1bc9beb249334bb6640c16153763a
Gloria Estrada <gloriaestra2@cecytab.edu.mx>
2026-04-25 22:39:43 -0600

    fix mysql connection railway

--- database.py
+++ database.py
@@ -5,13 +5,16 @@ def bd_teacheasy():
     try:
         conexion = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT", "26878")),
+            port=int(os.getenv("DB_PORT", 26878)),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
             ssl_disabled=False
         )
         return conexion
+
     except mysql.connector.Error as e:
-        print(e)
+        print("ERROR MYSQL:", e)
+        print("DB_HOST:", os.getenv("DB_HOST"))
+        print("DB_PORT:", os.getenv("DB_PORT"))
         return None
